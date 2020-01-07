55906e7bf803c763375d8366e0fc08fc36ac3777
pengcheng <pengcheng1161922543@gmail.com>
2020-01-08 00:53:44 +0800

    Nio 01.08

--- src/main/java/javase/nio/ChatRoomSelector.java
+++ src/main/java/javase/nio/ChatRoomSelector.java
@@ -2,6 +2,7 @@ package javase.nio;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.nio.channels.*;
 import java.util.Set;
 
@@ -24,13 +25,27 @@ public class ChatRoomSelector {
         serverSocketChannel.bind(inetSocketAddress);
         // 设置非阻塞式IO
         serverSocketChannel.configureBlocking(false);
+        // 注册到 Selector 上去
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
         while (selector.select() > 0){
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
             for (SelectionKey selectionKey : selectionKeys){
-                selectionKey.
+                // 从selector上的被选择的SelectionKey集合中删除正在处理的SelectionKey
+                selector.selectedKeys().remove(selectionKey);
                 SelectableChannel channel = selectionKey.channel();
-                channel.
+                // ServerSocket 有客户端连接进来
+                if (channel.validOps() == SelectionKey.OP_ACCEPT){
+                    ServerSocketChannel ServerSocketChannel = (ServerSocketChannel) channel;
+                    SocketChannel socketChannel = ServerSocketChannel.accept();
+                    // 设置成非阻塞模式
+                    socketChannel.configureBlocking(false);
+                    // 将socketChannel注册到 Selector 上去 (可读可写)
+                    socketChannel.register(selector,SelectionKey.OP_READ & SelectionKey.OP_WRITE);
+                    // 将serverSocketChannel 重新设置成
+                    selectionKey.interestOps(SelectionKey.OP_ACCEPT);
+                }
+
+
             }
 
             SocketChannel socketChannel = serverSocketChannel.accept();
